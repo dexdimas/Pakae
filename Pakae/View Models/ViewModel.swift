1dc9e63ba1893052e25041f7f7c5f30755faf633
Stendy Antonio <stendy20.sa@gmail.com>
2020-10-15 13:32:52 +0700

    Stendy View Model

---
+++ Pakae/View Models/ViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  ViewModel.swift
+//  Pakae
+//
+//  Created by Stendy Antonio on 14/10/20.
+//
+
+import SwiftUI
+
+final class ViewModel: ObservableObject{
+    private lazy var SignInWithApple = SignInWithAppleCoordinator()
+    @Published var user: User?
+    
+    func getRequest(){
+        SignInWithApple.getAppleRequest()
+    }
+    
+    func getUserInfo(){
+        if let userData = UserDefaults.standard.object(forKey: "user") as? Data,
+           let userDecode = try? JSONDecoder().decode(User.self, from: userData){
+            user = userDecode
+        }
+    }
+}
