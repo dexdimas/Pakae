fa09035a378c6852e4d156e37b08b3d44ec7eed3
Andimas Bagaswara <andimas.bagaswara@gmail.com>
2020-10-22 20:14:57 +0800

    [OB-F-14] Add TabBarPage

---
+++ Pakae/Screens/TabBarPage.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarPage.swift
+//  Pakae
+//
+//  Created by Andimas Bagaswara on 22/10/20.
+//
+
+import SwiftUI
+
+struct TabBarPage: View {
+    var body: some View {
+        TabView {
+            WelcomePage()
+                .tabItem {
+                    Image(systemName: "tray.fill")
+                    Text("Wardrobe")
+                }
+            SignInPage()
+                .tabItem {
+                    Image(systemName: "bookmark.fill")
+                    Text("Favorite")
+                }
+            SignUpScreen()
+                .tabItem {
+                    Image(systemName: "cloud.fill")
+                    Text("Inspiration")
+                }
+        }
+    }
+}
+
+struct TabBarPage_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarPage()
+    }
+}
