dfa84daf489894df41714a956480d34d728c89d6
Stendy Antonio <stendy20.sa@gmail.com>
2020-11-02 15:33:53 +0700

    Stendy Commit FIle

---
+++ Pakae/Models/UserSignInModel.swift
@@ -0,0 +1,17 @@
+//
+//  UserSignIn Model.swift
+//  Pakae
+//
+//  Created by Stendy Antonio on 29/10/20.
+//
+
+import Foundation
+import SwiftUI
+
+struct UserSignInModel: Codable {
+    
+    let access_token: String
+    let refresh_token: String
+    
+}
+
