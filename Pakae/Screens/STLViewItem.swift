31822b3df66f546f2693d6bd22832fcee4546d20
evanrenanto <62125175+evanrenanto@users.noreply.github.com>
2020-11-09 13:27:33 +0700

    Commit Bookmark, Reference, Direcnt to Store, Shop the look

--- Pakae/Screens/STLViewItem.swift
+++ Pakae/Screens/STLViewItem.swift
@@ -8,98 +8,107 @@
 import SwiftUI
 
 struct STLViewItem: View {
+    
     @State private var didTap1:Bool = true
-    @State private var didTap2:Bool = true
-    @State var bookmarkButton:String = "bookmark"
+    @State var bookmarkButton:String = "star"
     @State var tagButton:String = "tag"
     
     var body: some View {
         
-        VStack{
-            ZStack{
+        ScrollView(.vertical, showsIndicators: false) {
+            
+            VStack{
                 
-                Image("coba-7").resizable()
-                    .frame(width: 450, height: 450)
+                ZStack{
+                    
+                    Image("coba-5").resizable()
+                        .frame(width: 660, height: 665)
+                        .edgesIgnoringSafeArea(.top)
+                }
                 
-                VStack{
-                    Spacer()
-                    HStack{
-                        Button(action: {
-                            if(didTap1 == true) {
-                                self.tagButton = "tag.fill"
-                                didTap1 = false
-                            } else if (didTap1 == false) {
-                                self.tagButton = "tag"
-                                didTap1 = true
+                HStack{
+                    
+                    VStack{
+                        
+                        HStack{
+                            
+                            VStack{
+                                
+                                HStack{
+                                    
+                                    Image(systemName: "bag")
+                                    Text("Girl of the Month")
+                                        .font(.custom("Poppins-SemiBold", size: 15))
+                                        .foregroundColor(.gray)
+                                }
                             }
-                        }, label: {
-                            Image(systemName: tagButton).resizable().frame(width: 25, height: 25, alignment: .center)
-                        }).frame(width: 40, height: 40).background(Color.white).cornerRadius(30)
-                        Spacer()
+                            
+                            Spacer()
+                            
+                            Button(action: {
+                                if(didTap1 == true) {
+                                    self.bookmarkButton = "star.fill"
+                                    didTap1 = false
+                                } else if (didTap1 == false) {
+                                    self.bookmarkButton = "star"
+                                    didTap1 = true
+                                }
+                            }, label: {
+                                Image(systemName: bookmarkButton).resizable().frame(width: 35, height: 35, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            }).frame(width: 35, height: 35).background(Color.white).cornerRadius(30)
+                        }
+                        
+                        HStack{
+                            
+                            Text("Monochrome")
+                                .font(.custom("Poppins-SemiBold", size: 20))
+                            
+                            Spacer()
+                        }
+                        
+                        HStack{
+                            
+                            Text("Rp 749,900")
+                                .font(.custom("Poppins-Regular", size: 15))
+                                .foregroundColor(.gray)
+                            
+                            Spacer()
+                        }
                     }
-                }.frame(width: 250, height: 400)
-            }
-            
-            HStack{
-                VStack{
-                    HStack{
-                        Text("Monochrome").font(.title2).bold().foregroundColor(.red)
-                        Spacer()
-                    }
-                    HStack{
-                        Text("Meeting Attire Outfit by SevenSeas").font(.callout)
-                        Spacer()
-                    }
-                }
+                }.frame(width: 350)
+                .offset(y: -20)
                 
-                Button(action: {
-                    if(didTap2 == true) {
-                        self.bookmarkButton = "bookmark.fill"
-                        didTap2 = false
-                    } else if (didTap2 == false) {
-                        self.bookmarkButton = "bookmark"
-                        didTap2 = true
-                    }
-                }, label: {
-                    Image(systemName: bookmarkButton).resizable().frame(width: 20, height: 25, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                }).frame(width: 40, height: 40).background(Color.white).cornerRadius(30)
-            }.frame(width: 350)
-            
-            Spacer()
-            
-            HStack{
-                Text("Description").font(.title3)
                 Spacer()
-            }.frame(width: 350)
-            
-            HStack{
-                Text("The night that’s become dark again, My room that’s all square and straight, Feels like I’m trapped in it again, At that moment, something knocks on my window, The moonlight that shines on me, And curls around me").font(.caption)
+                
+                HStack{
+                    
+                    Text("Product Description")
+                        .font(.custom("Poppins-SemiBold", size: 20))
+                    Spacer()
+                }.frame(width: 350)
+                .offset(y: -20)
+                
+                HStack{
+                    
+                    Text("The night that’s become dark again, My room that’s all square and straight, Feels like I’m trapped in it again, At that moment, something knocks on my window, The moonlight that shines on me, And curls around me")
+                        .font(.custom("Poppins-Regular", size: 15))
+                    
+                    Spacer()
+                }.frame(width: 350)
+                .offset(y: -20)
+                
                 Spacer()
-            }.frame(width: 350)
-            
-            Spacer()
-            
-            HStack{
-                VStack{
-                    HStack{
-                        Text("Price").font(.title3)
-                        Spacer()
-                    }
-                }
-                Text("Rp. 69900").font(.callout).bold()
-            }.frame(width: 350)
-            
-            HStack{
-                NavigationView{
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("Buy Now")
-                            .frame(width: 350, height: 40)
-                            .foregroundColor(.white)
-                            .background(Color.red)
-                            .cornerRadius(10.0)
-                    })
-                }
-            }
+                
+                HStack{
+                    
+                }.frame(width: 350)
+                .offset(y: -20)
+                
+                Spacer()
+                
+                ViewOnWebsiteButton()
+                
+            }.offset(y: -75)
         }
     }
 }
---
+++ Pakae/Views/ViewOnWebsiteButton.swift
@@ -0,0 +1,28 @@
+//
+//  ViewOnWebsiteButton.swift
+//  Pakae
+//
+//  Created by Evan Renanto on 06/11/20.
+//
+
+import SwiftUI
+
+struct ViewOnWebsiteButton: View {
+    var body: some View {
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            
+            Text("View On Website")
+                
+                .frame(width: 350, height: 40)
+                
+                .foregroundColor(.white)
+                
+                .background(Color.gray)
+                
+                .cornerRadius(10.0)
+            
+        })
+    }
+}
+
+
