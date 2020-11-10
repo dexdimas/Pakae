faab04be5eef950f01b612fb23210fbecb17a873
Stendy Antonio <stendy20.sa@gmail.com>
2020-11-10 12:35:19 +0700

    Stendy add camera border

--- Pakae/Views/TakePicture For Detect/TakePicture/CameraInstructionView.swift
+++ Pakae/Views/TakePicture For Detect/TakePicture/CameraInstructionView.swift
@@ -10,35 +10,44 @@ import SwiftUI
 struct CameraInstructionView: View {
     
     var body: some View {
-        VStack{
+        ZStack{
             
-            Image("Tshirt").resizable()
-                
-                .frame(width: 250, height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Image("Camera Border").resizable().frame(width: UIScreen.main.bounds.width, height: 550, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             
-            Text("Make sure item is in the frame and not folded")
+            VStack{
                 
-                .multilineTextAlignment(.center)
                 
-                .foregroundColor(.white)
                 
-                .font(Font.system(size: 18))
+                Image("Tshirt").resizable()
+                    
+                    .frame(width: 250, height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 
-                .lineLimit(2)
+                Text("Make sure item is in the frame and not folded")
+                    
+                    .multilineTextAlignment(.center)
+                    
+                    .foregroundColor(.white)
+                    
+                    .font(Font.system(size: 18))
+                    
+                    .lineLimit(2)
+                    
+                    .padding(10)
                 
-                .padding(10)
+                Text("Tap anywhere to start")
+                    
+                    .multilineTextAlignment(.center)
+                    
+                    .foregroundColor(.white)
+                    
+                    .font(Font.system(size: 10))
+                
+                    .padding(.top,25)
+            }
+            .padding(28)
             
-            Text("Tap anywhere to start")
-                
-                .multilineTextAlignment(.center)
-                
-                .foregroundColor(.white)
-                
-                .font(Font.system(size: 10))
-            
-                .padding(.top,25)
         }
-        .padding(28)
+        
 
 //        .background(Color.gray.opacity(1))
 
